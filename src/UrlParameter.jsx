84c9e87d45e3ec986ff5271380b641b545570680
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2022-05-03 19:57:43 -0400

    useParams

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
 export const UrlParameter = () => {
+  const { id } = useParams();
   return (
     <div>
       <h1>UrlParameter</h1>
+      <p>パラメーターは{id}です</p>
     </div>
   );
 };
